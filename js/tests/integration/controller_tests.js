0e4b61a237f607541658b61f1d16f061edab5714
nstarke <starke.nicholas@gmail.com>
2014-09-18 09:43:56 -0500

    adding unit tests for imgUrl and correcting warning for bindAttr

--- js/tests/integration/controller_tests.js
+++ js/tests/integration/controller_tests.js
@@ -1,9 +1,10 @@
 import SpeakersController from "js/controllers/speakers";
+import SpeakerModel from "js/models/speaker";
 
 var speakers = Ember.A([]);
-var first = Ember.Object.create({name: "toran", bio: "javascript ninja", location: "Burlington, IA"});
-var middle = Ember.Object.create({name: "Iowa Code Camp", bio: "", location: ""});
-var last = Ember.Object.create({name: "nick", bio: "rockstar hacker", location: "Des Moines, IA"});
+var first = Ember.Object.create({name: "toran", bio: "javascript ninja", location: "Burlington, IA", img: "ToranBillups.jpg"});
+var middle = Ember.Object.create({name: "Iowa Code Camp", bio: "", location: "", img: ""});
+var last = Ember.Object.create({name: "nick", bio: "rockstar hacker", location: "Des Moines, IA", img: "NicholasStarke.jpg"});
 speakers.pushObject(first);
 speakers.pushObject(middle);
 speakers.pushObject(last);
@@ -26,4 +27,20 @@ test("should update the computed property when a speaker is modified", function(
     controller.get("content").pushObject(another);
     result = controller.get("speakers");
     equal(result.get("length"), 3);
+});
+
+test("should build the proper url for loading an image", function() {
+    var firstModel = SpeakerModel.create(first);
+    equal(firstModel.get("imgUrl"), "http://iowacodecamp.com/public/images/speakers/" + first.img);
+    var lastModel = SpeakerModel.create(last);
+    equal(lastModel.get("imgUrl"), "http://iowacodecamp.com/public/images/speakers/" + last.img);
+});
+
+test("should change url when img property is changed", function() {
+    var model = SpeakerModel.create(first);
+    var result = model.get("imgUrl");
+    equal(result, "http://iowacodecamp.com/public/images/speakers/" + first.img);
+    model.set("img", "BigBoss.jpg");
+    result = model.get("imgUrl");
+    equal(result, "http://iowacodecamp.com/public/images/speakers/BigBoss.jpg");
 });
